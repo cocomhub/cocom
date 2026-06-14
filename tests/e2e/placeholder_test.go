a2c2b2b967ccbd836a44ee831c816b77c8e6bdf2
suixibing <suixibing@gmail.com>
2026-06-14 15:37:17 +0800

    chore: remove placeholder test (replaced by main_test.go in same package)

--- tests/e2e/placeholder_test.go
+++
@@ -1,9 +0,0 @@
-// Copyright 2026 The Cocomhub Authors. All rights reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-package main
-
-import "testing"
-
-// placeholder for go mod tidy
-func TestPlaceholder(t *testing.T) {}
